a8cead84350dc2cb945db36319d12f7d155ce57c
FabrizioGrazioso <f.grazioso1@studenti.unisa.it>
2026-01-13 11:51:40 +0100

    .

--- LaCantinav2/src/main/java/it/unisa/lacantina/util/ConnectToDB.java
+++ LaCantinav2/src/main/java/it/unisa/lacantina/util/ConnectToDB.java
@@ -4,19 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectToDB
-{
-	private static final String URL = "jdbc:mysql://localhost:3306/LaCantina";
-	private static final String USER = "root";
-	private static final String PASSWORD = "Gb162004";
-	
-	// Metodo per ottenere la connessione al database
-	public static Connection getConnection() throws SQLException{
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return DriverManager.getConnection(URL, USER, PASSWORD);
-	}
+public class ConnectToDB {
+
+    private static final String DEFAULT_URL  = "jdbc:mysql://localhost:3306/LaCantina";
+    private static final String DEFAULT_USER = "root";
+    private static final String DEFAULT_PASS = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver non trovato", e);
+        }
+
+        String url  = System.getProperty("lacantina.db.url",  DEFAULT_URL);
+        String user = System.getProperty("lacantina.db.user", DEFAULT_USER);
+        String pass = System.getProperty("lacantina.db.pass", DEFAULT_PASS);
+
+        return DriverManager.getConnection(url, user, pass);
+    }
 }
