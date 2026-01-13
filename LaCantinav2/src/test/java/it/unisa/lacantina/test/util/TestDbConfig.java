adcbf1ffebfa888ad541e08c849ea31c3e6f6faf
FabrizioGrazioso <f.grazioso1@studenti.unisa.it>
2026-01-13 11:10:49 +0100

    Correzione POM

--- LaCantinav2/src/test/java/it/unisa/lacantina/test/util/TestDbConfig.java
+++ LaCantinav2/src/test/java/it/unisa/lacantina/test/util/TestDbConfig.java
@@ -5,7 +5,7 @@ public final class TestDbConfig {
 
     // Usa lo stesso DB del progetto (ConnectToDB), così non devi cambiare niente.
     // Se preferisci un DB separato, cambia qui e basta.
-    public static final String URL = "jdbc:mysql://localhost:3306/LaCantina";
+    public static final String URL = "jdbc:mysql://localhost:3306/LaCantina_test";
     public static final String USER = "root";
     public static final String PASSWORD = "";
 }
