7a6aa45bbcb2fd99df80a4b366ebeac0d5c4c280
SigsUni <g.cicalese21@studenti.unisa.it>
2026-01-14 17:02:08 +0100

    Ultime modifiche e creazione .war

--- LaCantinav2/src/main/java/it/unisa/lacantina/util/ConnectToDB.java
+++ LaCantinav2/src/main/java/it/unisa/lacantina/util/ConnectToDB.java
@@ -8,7 +8,7 @@ public class ConnectToDB {
 
     private static final String DEFAULT_URL  = "jdbc:mysql://localhost:3306/LaCantina";
     private static final String DEFAULT_USER = "root";
-    private static final String DEFAULT_PASS = "";
+    private static final String DEFAULT_PASS = "Gb162004";
 
     public static Connection getConnection() throws SQLException {
         try {
